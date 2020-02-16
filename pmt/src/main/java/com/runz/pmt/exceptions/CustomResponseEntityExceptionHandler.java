cbadafe31665c4c555afa87ffb542ef42b7cc12d
Runqing <runqingz@users.noreply.github.com>
2020-02-16 15:42:42 -0800

    Project Entity CRUD functions (#1)
    * Implement Project Entity CRUD functions

---
+++ pmt/src/main/java/com/runz/pmt/exceptions/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.runz.pmt.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleProjectIdException(ProjectIdException exception, WebRequest request) {
+        ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(exception.getMessage());
+
+        return new ResponseEntity<Object>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
