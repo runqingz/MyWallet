92ad926c670813bece1a89ad39fd0810a2463b94
Runqing <runqingz@users.noreply.github.com>
2020-02-17 11:21:06 -0800

    Rename packages (#3)
    rename packages

---
+++ pmtool/src/main/java/com/runz/pmtool/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,18 @@
+package com.runz.pmtool.exceptions;
+
+public class ProjectIdExceptionResponse {
+
+    private String projectIdentifier;
+
+    public ProjectIdExceptionResponse(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getProjectIdentifier() {
+        return this.projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+}
---
+++ pmtool/src/test/java/com/runz/pmtool/PersonalManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runz.pmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
