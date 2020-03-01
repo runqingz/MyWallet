5826417dcbaf1747d00d56d00ce31a4d4f57e192
Runqing <runqingz@users.noreply.github.com>
2020-03-01 14:47:44 -0800

    Update folder structure, update front end (#6)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
-import Dashboard from './components/Dashboard';
+import Dashboard from './web/components/Dashboard';
+import Header from './web/components/layout/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
     </div>
   );
--- src/components/project/ProjectItems.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function ProjectItems() {
-  return (
-    <h2>Project Item</h2>
-  );
-}
-
-export default ProjectItems;
