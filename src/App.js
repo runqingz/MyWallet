7147ff834c563abc7ad9858601193995f16dbdc9
Runqing Zhang <runqing.z@gmail.com>
2020-03-06 22:05:53 -0800

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import Dashboard from './web/components/Dashboard';
 import Header from './web/components/layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AddProject from './web/components/project/AddProject';
+import store from './store'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/web/components/project/NewProjectButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NewProjectButton = () => (
+  <>
+    <Link to="/addProject" href="ProjectForm.html" className="btn btn-lg btn-info">
+      Create a Project
+    </Link>
+  </>
+);
+
+
+export default NewProjectButton;
