5a2dcaa664733aeb995c07c9b688be4fbdf32c57
Runqing <runqingz@users.noreply.github.com>
2020-04-26 10:24:19 -0700

    Header UI update (#16)
    * Added full project CRUD operation
    
    * Header UI update

--- src/web/components/layout/Header.jsx
+++ src/web/components/layout/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+    <nav className="navbar navbar-expand-sm navbar-light bg-light mb-4">
       <div className="container">
         <a className="navbar-brand" href="Dashboard.html">
           Personal Management Tool
